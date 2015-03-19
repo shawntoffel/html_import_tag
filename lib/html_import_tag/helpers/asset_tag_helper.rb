bcaf691de6f40599717d5d52e5af332af5a5f805
Shawn Toffel <shawn.toffel@gmail.com>
2015-03-19 04:03:29 -0600

    Cleanup

--- lib/html_import_tag/helpers/asset_tag_helper.rb
+++ lib/html_import_tag/helpers/asset_tag_helper.rb
@@ -3,12 +3,12 @@ module ActionView
     module AssetTagHelper
       def html_import_tag(*sources)
         options = sources.extract_options!.stringify_keys
-
         path_options = options.extract!('protocol', 'extname').symbolize_keys
+
         sources.uniq.map do |source|
           tag_options = {
             rel: "import",
-            href: path_to_html(source, path_options)
+            href: path_to_asset(source, {:type => :html}.merge!(path_options))
           }.merge!(options)
           tag(:link, tag_options)
         end.join("\n").html_safe
